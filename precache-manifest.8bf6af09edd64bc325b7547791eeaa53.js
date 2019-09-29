5f9e194acc65205185832b92e260d0a5eee6a7a0
zhangtia <zhangtia@umich.edu>
2019-09-29 18:23:17 -0400

    Updates

---
+++ precache-manifest.8bf6af09edd64bc325b7547791eeaa53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f3d8ebdb6575cb367706a6dbcbc0eb",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "18e9167540d48d0bf200",
+    "url": "/page/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b3aa7a4649c73a33e9e8",
+    "url": "/page/static/js/2.1ff7f081.chunk.js"
+  },
+  {
+    "revision": "18e9167540d48d0bf200",
+    "url": "/page/static/js/main.19acdfe6.chunk.js"
+  },
+  {
+    "revision": "08d7a453b3de10982345",
+    "url": "/page/static/js/runtime-main.2d01f76c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/page/static/media/logo.25bf045c.svg"
+  }
+]);

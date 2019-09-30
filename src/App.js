e4bcdbdb6688341c345175ec601072b89a20ba0b
zhangtia <zhangtia@umich.edu>
2019-09-29 22:05:49 -0400

    tutorial finished

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { NoMatch } from './NoMatch';
 import { Layout } from './components/Layout';
 import { NavigationBar } from './components/NavigationBar';
 import { Jumbotron } from './components/Jumbotron';
- 
+
 function App() {
   return (
     <React.Fragment>

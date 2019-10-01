5460577d83d27caa0789a0704574a42ef5a213b7
zhangtia <zhangtia@umich.edu>
2019-10-01 14:57:56 -0400

    Cleaned up imports so deploy will not produce warnings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Nav, Navbar, Container, Row, Col } from 'react-bootstrap';
-import styled from 'styled-components';
-
+import { Nav, Navbar } from 'react-bootstrap';
 
 
 

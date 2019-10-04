93b32ba97b7f0eb08c7662bffd189c330d3ef8e6
zhangtia <zhangtia@umich.edu>
2019-10-04 17:46:04 -0400

    created resume page with temp download link

--- src/Contact.js
+++ src/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const Contact = () => (
     <div>
+        <h2>Download link just downloads a random PDF file currently</h2>
         <h2>Resume -
         <a href="https://github.com/zhangtia/resume/raw/master/pdftest.pdf" target="_blank"> Download</a>
         </h2><iframe src="https://docs.google.com/document/d/e/2PACX-1vSSfh007-Jqvl3tsgTOnDoisZEM4hT8A0CF-Ueu0pBHJGOwKO_oJrB1iLOool2BjlZEQ8_vJG4Turi5/pub?embedded=true" height="1200px" width="100%"></iframe>

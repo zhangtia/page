506e312d4b117c737e8188585dc51fc5995f8ad8
zhangtia <zhangtia@umich.edu>
2019-11-11 16:43:16 -0500

    this bettr work

--- src/Contact.js
+++ src/Contact.js
@@ -3,6 +3,6 @@ import res from './assets/Resume_Nov_11.pdf';
 
 export const Contact = () => (
     <div>
-        <p>Open a PDF file <a href="static/media/Res_Nov_11.pdf">example</a>.</p>
+        <p>Open a PDF file <a href="https://zhangtia.github.io/static/media/Res_Nov_11.pdf">example</a>.</p>
     </div>
 )

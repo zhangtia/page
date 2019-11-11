afec7fe2442de249f5246bded6a431932287ccf2
zhangtia <zhangtia@umich.edu>
2019-11-11 17:48:08 -0500

    finished front page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './Home';
 import { About } from './About';
-import { Contact } from './Contact';
 import { NoMatch } from './NoMatch';
 import { Layout } from './components/Layout';
-import { NavigationBar, FooterPage, Test } from './components/NavigationBar';
+import { NavigationBar, FooterPage } from './components/NavigationBar';
 import { Jumbotron } from './components/Jumbotron';
 //import { Footer } from './components/Footer';
 
@@ -22,7 +21,6 @@ function App() {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
             <Route component={NoMatch} />
           </Switch>
         </Router>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -26,92 +26,84 @@ const Styles = styled.div`
 export const NavigationBar = () => (
     <Styles>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="/page/#/">Tianqi Zhang</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto"></Nav>
-            <Nav>
-                <Nav.Link href="/page/#">Home</Nav.Link>
-                <Nav.Link href="/page/#/about">About</Nav.Link>
-                <Nav.Link href="https://zhangtia.github.io/static/media/Res_Nov_11.pdf" target="_blank">Resume</Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
+            <Navbar.Brand href="/page/#/">Tianqi Zhang</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto"></Nav>
+                <Nav>
+                    <Nav.Link href="/page/#">Home</Nav.Link>
+                    <Nav.Link href="/page/#/about">About</Nav.Link>
+                    <Nav.Link href="https://zhangtia.github.io/static/media/Res_Nov_11.pdf" target="_blank">Resume</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     </Styles>
 )
 
 
 export const FooterPage = () => {
-  return (
-    <MDBFooter color="elegant-color" className="font-small darken-3 pt-0">
-      <MDBContainer>
-        <MDBRow>
-          <MDBCol md="12" className="py-5">
-            <div className="mb-5 flex-center">
-              <a className="github-ic">
-                <i className="fab fa-github fa-lg white-text mr-md-5 mr-3 fa-2x">
-                </i>
-              </a>
-              <a className="tw-ic">
-                <i className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x">
-                </i>
-              </a>
-              <a className="gplus-ic">
-                <i className="fab fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x">
-                </i>
-              </a>
-              <a className="li-ic">
-                <i className="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x">
-                </i>
-              </a>
-              <a className="ins-ic">
-                <i className="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x">
-                </i>
-              </a>
-              <a className="pin-ic">
-                <i className="fab fa-pinterest fa-lg white-text fa-2x"> </i>
-              </a>
+    return (
+        <MDBFooter color="elegant-color" className="font-small darken-3 pt-0">
+            <MDBContainer>
+                <MDBRow>
+                    <MDBCol md="12" className="py-5">
+                        <div className="mb-5 flex-center">
+                            <a className="github-ic justify-content-center" href="https://github.com/zhangtia">
+                                <i className="fab fa-github white-text mr-md-5 mr-3 fa-2x">
+                                </i>
+                            </a>
+                            <a className="email-ic justify-content-center" href="mailto: zhangtia@umich.edu">
+                                <i className="far fa-envelope white-text mr-md-5 mr-3 fa-2x">
+                                </i>
+                            </a>
+                            <a className="li-ic justify-content-center" href="https://www.linkedin.com/in/tqzmatt/">
+                                <i className="fab fa-linkedin-in white-text mr-md-5 mr-3 fa-2x">
+                                </i>
+                            </a>
+                            <a className="inst-ic justify-content-center" href="https://www.instagram.com/m4ttq/">
+                                <i className="fab fa-instagram white-text mr-md-5 mr-3 fa-2x">
+                                </i>
+                            </a>
+                        </div>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+            <div className="footer-copyright text-center py-3">
+                <MDBContainer fluid>
+                    &copy; {new Date().getFullYear()} Made with MDBootstrap and React
+                </MDBContainer>
             </div>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-      <div className="footer-copyright text-center py-3">
-        <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright:{" "}
-          <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
-        </MDBContainer>
-      </div>
-    </MDBFooter>
-  );
+        </MDBFooter>
+    );
 }
 
 export const Footer = () => (
-<footer style={{marginTop:"auto"}}>
-    <header class="page-header">This is the header</header>
-    <article class="page-body">
-        <p>Main page content here, add more if you want to see the footer push down.</p>
-    </article>
-    <footer class="page-footer">Sticky footer</footer>
-</footer>
+    <footer style={{ marginTop: "auto" }}>
+        <header class="page-header">This is the header</header>
+        <article class="page-body">
+            <p>Main page content here, add more if you want to see the footer push down.</p>
+        </article>
+        <footer class="page-footer">Sticky footer</footer>
+    </footer>
 
 )
 
 export const Test = () => (
     <Styles>
         <Navbar class="ftr" collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="bottom">
-        
-        <Navbar.Collapse id="basic-navbar-nav">
+
+            <Navbar.Collapse id="basic-navbar-nav">
                 <Container>
-                <Row className="justify-content-md-center">
-                <Col xs lg="2"><Nav.Link href="https://github.com/zhangtia">GITHUB</Nav.Link></Col>
-                <Col xs lg="2"><Nav.Link href="mailto: zhangtia@umich.edu">EMAIL</Nav.Link></Col>
-                <Col xs lg="2"><Nav.Link href="/page/#/contact">Contact</Nav.Link></Col>
-                </Row>
+                    <Row className="justify-content-md-center">
+                        <Col xs lg="2"><Nav.Link href="https://github.com/zhangtia">GITHUB</Nav.Link></Col>
+                        <Col xs lg="2"><Nav.Link href="mailto: zhangtia@umich.edu">EMAIL</Nav.Link></Col>
+                        <Col xs lg="2"><Nav.Link href="/page/#/contact">Contact</Nav.Link></Col>
+                    </Row>
                 </Container>
-            
-        </Navbar.Collapse>
+
+            </Navbar.Collapse>
         </Navbar>
     </Styles>
 
-    
+
 )

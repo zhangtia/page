60a67946d93e08ade3333a1d79f6446e1aa586e1
zhangtia <zhangtia@umich.edu>
2019-12-26 18:26:14 -0500

    add page

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './Projects.css';
 
 export const Project = () => {
     <div>
-        Project page
+        <h4>Project page</h4>
+        <div class="left">
+            Capture the Breaker Tournament Generator - Personal project
+        </div>
+        <div class="right">
+            Created a simple app using Spring Boot and React. 
+        </div>
     </div>
 }

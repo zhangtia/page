6bc028e523b96a1862c960c20cd410689fa3de8d
zhangtia <zhangtia@umich.edu>
2019-12-26 19:13:28 -0500

    add page

--- src/Projects.js
+++ src/Projects.js
@@ -4,12 +4,17 @@ import './Projects.css';
 export const Project = () => (
     <div>
         <div class="titlep">
-            <h2>Project page</h2>
+            <h2>Welcome to my Project Page!</h2>
         </div>
         <div class="leftp">
-            <h4>Capture the Breaker Tournament Generator - Personal project</h4>
+            <h4>Capture the Breaker! Tournament Generator</h4>
+            <h5>Personal Project</h5>
         </div>
         <div class="rightp">
+            <ul>
+                <li>Created a webapp for annual "Capture the Breaker!" breakdance competition by our dance group, Element 1</li>
+                <li>Noticed tournament format could not be found online, and a crewmate wrote his own algorithm for seeding and used paper for recording</li>
+            </ul>
             Created a simple app using Spring Boot and React. 
         </div>
     </div>

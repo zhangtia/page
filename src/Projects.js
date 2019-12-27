3f66c852144a09c0ae7d5f068a96aef31879af34
zhangtia <zhangtia@umich.edu>
2019-12-27 01:06:50 -0500

    add page

--- src/Projects.js
+++ src/Projects.js
@@ -18,12 +18,21 @@ export const Project = () => (
             </ul>
         </div>
         <div class="whyp">
-                Why?
+            Why?
             </div>
-            <div class="whyansp">
-                Capture the Breaker has a unique format where the winning team will 'capture' a member from the losing team and progress.
-                Therefore, to ensure the finals consists of the best (top 10) breakers, a crewmate wrote his own algorithm based on the preliminary round scores.
-                That intrigued me and I could not find a tournament generator with our format. So I asked myself: why not combine two of my interests, breaking and coding, in one project?
+        <div class="whyansp">
+            Capture the Breaker has a unique format: The winning team will 'capture' a member from the losing team and progress.
+            To ensure the finals consisted of the best (top 10) breakers, a crewmate wrote his own algorithm based on the preliminary seeding.
+            I decided to write a webapp to integrate the algorithm and automate the tournament process. This project combines two of my interests: Coding and Breaking
             </div>
+        <div class="wherep">
+            Where?
+            </div>
+        <div class="whereansp">
+            Check out the project! First deployment after 30 mins of inactivity might take a while, be patient!
+            </div>
+        <div class="wherebtn">
+            <Button href="https://morning-depths-35258.herokuapp.com/" target="_blank">Capture the Breaker!</Button>
+        </div>
     </div>
 )

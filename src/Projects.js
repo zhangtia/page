e89fbe7da73c82e6676ced56399d37a867e8f67c
zhangtia <zhangtia@umich.edu>
2019-12-27 01:12:03 -0500

    add page

--- src/Projects.js
+++ src/Projects.js
@@ -18,22 +18,26 @@ export const Project = () => (
                 <li>Will implement other battle formats as we organise more competitions (e.g. 7 to smoke)</li>
             </ul>
         </div>
-        <div class="whyp">
-            Why?
+        <div class="whyall">
+            <div class="whyp">
+                Why?
             </div>
-        <div class="whyansp">
-            Capture the Breaker has a unique format: The winning team will 'capture' a member from the losing team and progress.
-            To ensure the finals consisted of the best (top 10) breakers, a crewmate wrote his own algorithm based on the preliminary seeding.
-            I decided to write a webapp to integrate the algorithm and automate the tournament process. This project combines two of my interests: Coding and Breaking
+            <div class="whyansp">
+                Capture the Breaker has a unique format: The winning team will 'capture' a member from the losing team and progress.
+                To ensure the finals consisted of the best (top 10) breakers, a crewmate wrote his own algorithm based on the preliminary seeding.
+                I decided to write a webapp to integrate the algorithm and automate the tournament process. This project combines two of my interests: Coding and Breaking
             </div>
-        <div class="wherep">
-            Where?
+        </div>
+        <div class="whereall">
+            <div class="wherep">
+                Where?
             </div>
-        <div class="whereansp">
-            Check out the project! First deployment after 30 mins of inactivity might take a while, be patient!
+            <div class="whereansp">
+                Check out the project! First deployment after 30 mins of inactivity might take a while, be patient!
             </div>
-        <div class="wherebtn">
-            <Button href="https://morning-depths-35258.herokuapp.com/" target="_blank">Capture the Breaker!</Button>
+            <div class="wherebtn">
+                <Button href="https://morning-depths-35258.herokuapp.com/" target="_blank">Capture the Breaker!</Button>
+            </div>
         </div>
     </div>
 )

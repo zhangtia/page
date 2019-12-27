bc6d385b5c57b3a042a7a64dc9dcfdcc0b4f37ee
zhangtia <zhangtia@umich.edu>
2019-12-26 23:26:15 -0500

    add page

--- src/Projects.js
+++ src/Projects.js
@@ -13,9 +13,16 @@ export const Project = () => (
         <div class="rightp">
             <ul>
                 <li>Created a webapp for annual "Capture the Breaker!" breakdance competition by our dance group, Element 1</li>
-                <li>Noticed tournament format could not be found online, and a crewmate wrote his own algorithm for seeding and used paper for recording</li>
+                <li>Utilized Spring Boot to create a back-end microservice, front-end implemented by React and deployed to Heroku</li>
+                <li>Webapp handles logistics fo the tournament: sign-up, preliminary and knockout stages</li>
             </ul>
-            Created a simple app using Spring Boot and React. 
+            <div class="whyp">
+                Why?
+            </div>
+            <div class="whyansp">
+                Created a simple app using Spring Boot and React.
+                Noticed tournament format could not be found online, and a crewmate wrote his own algorithm for seeding and used paper for recording
+            </div>
         </div>
     </div>
 )

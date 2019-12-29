b982e6f5a6cc4495e428999acf5c2cc7377edee5
zhangtia <zhangtia@umich.edu>
2019-12-29 01:35:51 -0500

    new

--- src/Projects.js
+++ src/Projects.js
@@ -17,7 +17,7 @@ export const Project = () => (
             <div class="rightp">
                 <ul>
                     <li>Created a webapp for annual "Capture the Breaker!" breakdance competition by our dance group, Element 1</li>
-                    <li>Utilized Spring Boot to create a back-end microservice, front-end implemented by React and deployed to Heroku</li>
+                    <li>Utilized <b>Spring Boot</b> to create a back-end microservice, front-end implemented by <b>React</b> and deployed to <b>Heroku</b></li>
                     <li>Webapp handles logistics fo the tournament: sign-up, preliminary and knockout stages</li>
                     <li>Will implement other battle formats as we organise more competitions (e.g. 7 to smoke)</li>
                 </ul>
@@ -37,7 +37,7 @@ export const Project = () => (
                 Where?
             </div>
             <div class="whereansp">
-                <p>Check out <a href="https://morning-depths-35258.herokuapp.com/" target="_blank" rel="noopener noreferrer">Tournament Generator</a> with USER: admin and PASS: admin123! Initial deployment will take a while, please be patient!</p>
+                <p>Check out <a href="https://morning-depths-35258.herokuapp.com/" target="_blank" rel="noopener noreferrer">Tournament Generator</a> with <u>USER: admin</u> and <u>PASS: admin123</u>! Initial deployment will take a while, please be patient!</p>
                 <p>Check out our 2019 Capture the Breaker <a href="https://www.facebook.com/events/807989939615445/" target="_blank" rel="noopener noreferrer">Facebook Page</a> and the <a href="https://youtu.be/nXCfDSTv3BY" target="_blank" rel="noopener noreferrer">Final Battle!</a></p>
             </div>
         </div>

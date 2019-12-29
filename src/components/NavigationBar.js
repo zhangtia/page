26f73abced385322373faf1eaa7b40a624dbc058
zhangtia <zhangtia@umich.edu>
2019-12-29 17:25:02 -0500

    new

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -34,7 +34,7 @@ export const NavigationBar = () => (
                     <Nav.Link href="/page/#">Home</Nav.Link>
                     <Nav.Link href="/page/#/about">About</Nav.Link>
                     <Nav.Link href="/page/#/projects">Projects</Nav.Link>
-                    <Nav.Link href="https://zhangtia.github.io/static/media/Res_Nov_11.pdf" target="_blank">Resume</Nav.Link>
+                    <Nav.Link href="https://zhangtia.github.io/static/media/MatthewTQZhang.pdf" target="_blank">Resume</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
